04db8f4db349cf0f71ce5e63965c8b73eb34ee54
Ami Turgman <amitu@microsoft.com>
2017-07-20 21:49:30 +0300

    updating contract calls code

--- services/contract.js
+++ services/contract.js
@@ -11,26 +11,34 @@ var contractInstance = web3.eth.contract(abi).at(config.CONTRACT_ADDRESS);
 
 // wrap following functions with a promise so that we can use it with the async convention
 // and add it on the module.exports object
+// TODO: auto generate from abi
 [ 
-  'getProof',
-  'startTracking',
-  'storeProof',
-  'transfer'
+  { name: 'getProof', method: 'call' },
+  { name: 'startTracking', method: 'sendTransaction' },
+  { name: 'storeProof', method: 'sendTransaction' },  
+  { name: 'transfer', method: 'sendTransaction' }
 ]
-  .forEach(fname => {
+  .forEach(func => {
 
-    (function(fname) {
-      exports[fname] = function() {
+    (function(func) {
+      exports[func.name] = function() {
         return new Promise((resolve, reject) => {
-          console.log(`executing function '${fname}' on contract with params ${util.inspect(arguments)}`);
+          console.log(`executing function '${func.name}' with '${func.method}' call on contract with params ${util.inspect(arguments)}`);
 
           // callback for the function invoked
           var cb = (err, result) => {
             if (err) {
-              console.error(`error executing function '${fname}' on contract with params: ${util.inspect(arguments)}: ${err.message}`);
+              console.error(`error executing function '${func.name}' on contract with params: ${util.inspect(arguments)}: ${err.message}`);
               return reject(err);
             }
-            console.log(`function '${fname}' on contract completed successfully. result: ${util.inspect(result)}`);
+            
+            console.log(`function '${func.name}' on contract completed successfully`);
+
+            if (func.method === 'call')
+              console.log(`result: ${result.toString()}`);
+            else
+             console.log(`tx hash: ${result}`);
+            
             return resolve(result);
           };
 
@@ -39,10 +47,13 @@ var contractInstance = web3.eth.contract(abi).at(config.CONTRACT_ADDRESS);
           params.push(cb);
 
           // invoke requested method on the contract
-          return contractInstance[fname].apply(contractInstance, params);
+          // https://ethereum.stackexchange.com/questions/765/what-is-the-difference-between-a-transaction-and-a-call
+          // TODO: in case of sendTransaction, we need to use Events to get the actual result after the transaction is mined
+          // when using 'call' the result will be returned immediately. This is only for contant functions or such that do not change the state.
+          return contractInstance[func.name][func.method].apply(contractInstance, params);
         });
       }
-    })(fname);
+    })(func);
 
 });
 
