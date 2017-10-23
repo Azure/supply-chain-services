aa93f06e13c12ddb4356a35909c1fb1131bfc3c2
rabee333 <rabee333@gmail.com>
2017-10-23 15:43:25 +0300

    Request Authorization task

--- api/proof.js
+++ api/proof.js
@@ -13,7 +13,6 @@ var app = express();
 app.get('/:trackingId', async (req, res) => {
 
   req.checkParams('trackingId', 'Invalid trackingId').notEmpty();
-  req.checkQuery('userId', 'Invalid userId').notEmpty();
 
   var errors = await req.getValidationResult();
   if (!errors.isEmpty()) {
@@ -21,7 +20,7 @@ app.get('/:trackingId', async (req, res) => {
   }
 
   var trackingId = decodeURIComponent(req.params.trackingId);
-  var userId = decodeURIComponent(req.query.userId);
+  var userId = req.headers['user-id'];
   
   var opts = { 
     trackingId, 
